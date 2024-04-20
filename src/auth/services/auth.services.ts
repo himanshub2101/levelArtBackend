50c5b1a098d3f6bb400b0e37756bae8529b0ba67
ritikaverma5655 <ritikaverma5655@gmail.com>
2024-04-20 16:13:22 +0530

    fix bugs

--- src/auth/services/auth.services.ts
+++ src/auth/services/auth.services.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { UserService } from "src/users/services/user.services";
 import { successMessage } from "src/utils/response.util";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthServices {
@@ -10,16 +11,21 @@ export class AuthServices {
     private jwtService: JwtService
   ) {}
 
-  async signIn(username: string, pass: string): Promise<{ access_token: string, message: any }> {
+  async signIn(username: string, password: string): Promise<{ access_token: string, message: any }> {
     try {
-      let user = await this.usersService.findOneById(username);
+      let user = null;
 
-      if (!user) {
+      // Check if the username is a valid ObjectId (for _id) or an email address
+      if (username.match(/^[0-9a-fA-F]{24}$/)) {
+        // If it's an ObjectId, search by _id
+        user = await this.usersService.findOneById(username);
+      } else {
+        // If it's not an ObjectId, search by email
         user = await this.usersService.findOneByEmail(username);
       }
 
-      if (!user || user.password !== pass) {
-        throw new UnauthorizedException();
+      if (!user || !(await bcrypt.compare(password, user.password))) {
+        throw new UnauthorizedException('Invalid credentials');
       }
 
       const payload = { sub: user._id, username: user.email };
